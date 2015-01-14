45d723f3ecce378dc351fe782768735907af4648
sqeezy <sqeezy@gmx.de>
2015-01-14 21:22:06 +0100

    Started Implementation

--- AutomatedNullChecks.Sandbox/Properties/AssemblyInfo.cs
+++ AutomatedNullChecks.Sandbox/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AutomatedNullChecks")]
+[assembly: AssemblyTitle("AutomatedNullChecks.Sandbox")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AutomatedNullChecks")]
+[assembly: AssemblyProduct("AutomatedNullChecks.Sandbox")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("78d27643-3fe2-4448-96d6-7efaec0beb4c")]
+[assembly: Guid("30a5aa7d-a24f-47ab-98af-efcf2de011e8")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,4 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("AutomatedNullChecks.Sandbox")]
