2ec8688291f7f65e74e9105e9e1b2eca67f1faf8
Anton Herzog <sqeezy@gmx.de>
2015-01-15 09:48:48 +0100

    First implementation.

--- AutomatedNullChecks.Sandbox/Program.cs
+++ AutomatedNullChecks.Sandbox/Program.cs
@@ -10,6 +10,57 @@ namespace AutomatedNullChecks.Sandbox
     {
         static void Main(string[] args)
         {
+            TesterClass o;
+            ConstrutorNullCheckUtils.TestAllNullArgumentExceptionsForConstructors(typeof (TesterClass));
+        }
+
+        public class TesterClass
+        {
+            private readonly int _number;
+            private readonly ArgumentClass _simpleClass;
+            private readonly IDisposable _someInterface;
+            private readonly ArgumentClassComplex _complexClass;
+
+            public TesterClass(int number,ArgumentClass simpleClass,IDisposable someInterface,ArgumentClassComplex complexClass)
+            {
+                _number = number;
+                _simpleClass = simpleClass;
+                _someInterface = someInterface;
+                _complexClass = complexClass;
+            }
+
+            public int Number
+            {
+                get { return _number; }
+            }
+
+            public ArgumentClass SimpleClass
+            {
+                get { return _simpleClass; }
+            }
+
+            public IDisposable SomeInterface
+            {
+                get { return _someInterface; }
+            }
+
+            public ArgumentClassComplex ComplexClass
+            {
+                get { return _complexClass; }
+            }
+        }
+
+        public class ArgumentClass
+        {
+            public int SomeProperty { get; set; }
+        }
+
+        public class ArgumentClassComplex
+        {
+            public ArgumentClassComplex(int y)
+            {
+                //something
+            }
         }
     }
 }
--- AutomatedNullChecks/ConstrutorNullCheckUtils.cs
+++ AutomatedNullChecks/ConstrutorNullCheckUtils.cs
@@ -8,9 +8,9 @@ namespace AutomatedNullChecks
 {
     public static class ConstrutorNullCheckUtils
     {
-        public static void TestAllNullArgumentExceptionsForConstructors<T>(T type) where T : class
+        public static void TestAllNullArgumentExceptionsForConstructors(Type type)
         {
-            IEnumerable<ConstructorInfo> constructors = type.GetType().GetConstructors();
+            IEnumerable<ConstructorInfo> constructors = type.GetConstructors();
             foreach (ConstructorInfo constructor in constructors)
             {
                 TestForNullArgumentException(constructor);
@@ -20,15 +20,27 @@ namespace AutomatedNullChecks
         private static void TestForNullArgumentException(ConstructorInfo constructor)
         {
             IEnumerable<ParameterInfo> parameters = constructor.GetParameters();
+            IEnumerable<object> validParams = parameters.Select(GetBasicImplementation);
+            var x = constructor.Invoke(validParams.ToArray());
+            x.GetHashCode();
         }
 
-        //private static object GetBasicImplementation(ParameterInfo parameter)
-        //{
-        //    Type type = parameter.GetType();
-        //    if (type.IsAbstract) { throw  new InvalidOperationException("Abstract types are not supported.");}
-        //    if (type.IsInterface)
-        //    {
-        //    }
-        //}
+        private static object GetBasicImplementation(ParameterInfo parameter)
+        {
+            Type type = parameter.ParameterType;
+            if (type.IsInterface)
+            {
+                return InterfaceObjectFactory.CreateInterfaceImplementation(type);
+            }
+            if (type.IsAbstract)
+            {
+                throw new InvalidOperationException("Abstract types are not supported.");
+            }
+            if (type.IsValueType)
+            {
+                return Activator.CreateInstance(type);
+            }
+            return Activator.CreateInstance(type);
+        }
     }
 }
